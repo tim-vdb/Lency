b709af5bc7400452a0ba2b556cf87c193144f7d1
Guerric Co <cochelin.guerric@hotmail.com>
2025-09-24 11:05:16 +0200

    add inscription events form

--- src/features/SignUpEvents/components/SignUpEvents.tsx
+++ src/features/SignUpEvents/components/SignUpEvents.tsx
@@ -5,16 +5,108 @@ import { useForm } from "react-hook-form";
 import { z } from "zod"
 import { useRouter } from "next/navigation";
 import { EventsFormSchema } from "@/features/SignUpEvents/events.schema";
+import { EventsSafeAction } from "../events.action";
+import { useAction } from "next-safe-action/hooks";
+import { toast } from "sonner";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-export default function SignUpForm() {
+export default function SignUpEvents() {
     const router = useRouter()
     // 1. Define your form.
     const form = useForm<z.infer<typeof EventsFormSchema>>({
         resolver: zodResolver(EventsFormSchema),
         defaultValues: {
-            firstName: "",
-            lastName: "",
+            name: "",
             email: "",
-            password: "",
+            phone: "",
         },
-    })}
+    });
+
+    const { executeAsync, hasErrored, result, hasSucceeded } = useAction(EventsSafeAction, {
+        onSuccess: (data) => {
+            toast.success("Vous êtes inscrit à l'event");
+            form.reset();
+            router.refresh();
+        },
+        onError: (error) => {
+            toast.error(error.error.serverError || "Une erreur est survenue");
+        }
+    });
+
+    // 2. Define a submit handler.
+        async function onSubmit(values: z.infer<typeof EventsFormSchema>) {
+            // Do something with the form values.
+            // ✅ This will be type-safe and validated.
+            // console.log(values);
+            await executeAsync(values);
+        }
+
+    return (
+        <div className="bg-blue-50 dark:bg-gray-800 py-12">
+            <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-4">
+                        Rejoindre un évènement
+                    </h2>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Nom</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Nom" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Votre nom complet.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Email" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Votre email.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="phone"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Nom</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Nom" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Votre nom complet.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">S'inscrire à l'évènement</Button>
+                        </form>
+                    </Form>
+                    {hasErrored && <p className="text-red-500">{result?.serverError}</p>}
+                    {hasSucceeded && <p className="text-green-500">Vous êtes inscrit à la newsletter {result?.data?.name}</p>}
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/features/SignUpEvents/events.schema.ts
+++ src/features/SignUpEvents/events.schema.ts
@@ -1,8 +1,7 @@
 import z from "zod";
 
 export const EventsFormSchema = z.object({
-    firstName: z.string().min(2, { message: "Le prénom est requis d'au moins 2 caractères" }).max(50, { message: "Le prénom ne doit pas dépasser 50 caractères" }),
-    lastName: z.string().min(2, { message: "Le nom est requis d'au moins 2 caractères" }).max(50, { message: "Le nom ne doit pas dépasser 50 caractères" }),
+    name: z.string().min(2, { message: "Le nom est requis d'au moins 2 caractères" }).max(50, { message: "Le nom ne doit pas dépasser 50 caractères" }),
     email: z.string().email(),
-    password: z.string(),
+    phone: z.string().optional(),
 })
