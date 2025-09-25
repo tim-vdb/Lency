3f95ae5da70603dc227144c4c9d36fe3aa67953c
Guerric Co <cochelin.guerric@hotmail.com>
2025-09-25 09:04:28 +0200

    footer center

--- src/app/(site)/events/[id]/page.tsx
+++ src/app/(site)/events/[id]/page.tsx
@@ -1,9 +1,17 @@
+import { Card, CardContent } from "@/components/ui/card";
 import SignUpEvents from "@/features/Events/SignUpEvents/components/SignUpEvents";
 
 export default function page() {
   return (
-    <div className="w-[90%] m-auto">
+    <div className=" flex justify-center p-15">
+      <Card>
+       
+        <CardContent>
       <SignUpEvents />
+    </CardContent>
+        
+        
+      </Card>
     </div>
   );
 }
