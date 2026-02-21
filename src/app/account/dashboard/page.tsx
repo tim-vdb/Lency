cc86cd666859309353e0cf80c4797dde1912c40d
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-02-21 18:25:35 +0100

    feat(MarketplaceBlock): marketplace projects & members added

--- src/app/account/dashboard/page.tsx
+++ src/app/account/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import PostsBlock from '@/front/components/Public/Dashboard/PostBlock/PostsBlock'
+import MarketplaceBlock from '@/front/components/Public/Dashboard/Marketplace/MarketplaceBlock'
+import PostsBlock from '@/front/components/Public/Dashboard/Posts/PostsBlock'
 import {
     Card
 } from '@/front/components/ui/card'
@@ -8,7 +9,7 @@ export default function Dashboard() {
         // bg-neutral-150
         <div className='grid grid-cols-8 grid-rows-8 gap-6 p-4 h-screen bg-[#FCF5EA]'>
             <PostsBlock />
-            <Card className="col-[1/5] row-[5/9] py-0"></Card>
+            <MarketplaceBlock />
             <Card className="col-[5/7] row-[5/9] "></Card>
             <Card className="col-[7/9] row-[1/2] shadow-lg-base"></Card>
             <Card className="col-[7/9] row-[2/7]"></Card>
---
+++ src/front/components/Public/Dashboard/Marketplace/MarketplaceBlock.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/front/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/front/components/ui/card";
+
+const marketplaceData = [
+    {
+        category: "Nos projets",
+        items: [
+            {
+                title: "Rejoindre un projet",
+                description: "Découvrez les projets audio & visuel en cours qui recrutent activement des talents comme le vôtre.",
+                buttons: [
+                    { label: "Découvrir les projets" },
+                    { label: "Déposer ma candidature" }
+                ]
+            },
+            {
+                title: "Lancer votre projet",
+                description: "Créez votre projet audio/visuel, définissez vos besoins et recrutez votre équipe au sein de la communauté.",
+                buttons: [
+                    { label: "Créer un projet" },
+                    { label: "Voir les exemples" }
+                ]
+            }
+        ]
+    },
+    {
+        category: "Nos membres",
+        items: [
+            {
+                title: "Recruter des talents",
+                description: "Trouvez rapidement les profils spécialisés dont vous avez besoin : monteur, sound designer, comédien, etc.",
+                buttons: [
+                    { label: "Parcourir les membres" },
+                    { label: "Publier une offre" }
+                ]
+            },
+            {
+                title: "Valoriser mon profil",
+                description: "Valorisez vos compétences, mettez en avant vos réalisations et soyez contacté(e) pour des collaborations.",
+                buttons: [
+                    { label: "Compléter mon profil" },
+                    { label: "Aperçu des opportunités" }
+                ]
+                // TODO: Au début "Compléter" puis "Optimiser" une fois que le profil est complété
+            }
+        ]
+    }
+];
+
+export default function MarketplaceBlock() {
+    return (
+        <Card className="col-[1/5] row-[5/9] justify-between gap-0 border border-neutral-400">
+            <CardHeader className="px-4">
+                <CardTitle>Nos marketplaces</CardTitle>
+                <CardDescription>Connectez-vous aux projets et aux créateurs</CardDescription>
+            </CardHeader>
+            <CardContent className="grid grid-cols-2 gap-5 px-4">
+                {marketplaceData.map((section, sectionIndex) => (
+                    <Card key={sectionIndex} className="border-none shadow-none gap-1 text-neutral-400 py-0">
+                        <CardHeader className="px-0">
+                            <CardTitle className="text-xs">{section.category}</CardTitle>
+                        </CardHeader>
+                        <CardContent className="flex flex-col gap-4 px-0">
+                            {section.items.map((item, itemIndex) => (
+                                <Card key={itemIndex} className="py-3 gap-2 border bg-neutral-150 shadow-lg-base">
+                                    <CardHeader>
+                                        <CardTitle className="text-sm">{item.title}</CardTitle>
+                                    </CardHeader>
+
+                                    <CardContent className="line-clamp-2 text-xs">
+                                        {item.description}
+                                    </CardContent>
+
+                                    <CardFooter className="grid grid-cols-2 gap-2 px-4">
+                                        {item.buttons.map((button, buttonIndex) => (
+                                            <Button
+                                                key={buttonIndex}
+                                                variant="outline"
+                                                size="sm"
+                                                className="border p-1 bg-neutral-300 cursor-pointer text-xs"
+                                            >
+                                                {button.label}
+                                            </Button>
+                                        ))}
+                                    </CardFooter>
+                                </Card>
+                            ))}
+                        </CardContent>
+                    </Card>
+                ))}
+            </CardContent>
+        </Card>
+    );
+}
