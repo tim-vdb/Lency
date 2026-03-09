38de9d3567900951a719b938cb0b86f5bb26d9e4
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-03-09 23:28:17 +0100

    hotfix(AuthClient)

--- src/back/lib/auth-client.ts
+++ src/back/lib/auth-client.ts
@@ -1,10 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 
-const baseURL = process.env.BASE_URL || "http://localhost:3000"
-
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: baseURL
-})
+export const authClient = createAuthClient({})
 
 export const { signIn, signUp, signOut, useSession } = authClient;
