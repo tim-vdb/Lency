01ba317568c5b9d3172c6c1dc0c203ee037b7aa5
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-03-09 22:06:42 +0100

    fix(suspense-useSearchParams)

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -1,11 +1,12 @@
-'use client';
-
+import { Suspense } from 'react';
 import LoginForm from '@/front/components/Private/Auth/LoginForm';
 
 export default function SignIn() {
   return (
     <div className=" flex justify-center p-15">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
