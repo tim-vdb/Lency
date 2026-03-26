a5baaf6b079dbfadf2c8a4ce30c3e4990656ca44
Timothée Van Den Bosch <timotheevdbosch@gmail.com>
2026-03-26 07:52:30 +0100

    feat(sidebar-header): fixed on pages (not /account & /admin)

--- src/front/components/Private/Account/AccountShell.tsx
+++ src/front/components/Private/Account/AccountShell.tsx
@@ -6,18 +6,21 @@ import { SidebarInset, SidebarProvider } from "@/front/components/ui/sidebar"
 import { UserProvider } from "@/front/context/UserContext"
 import type { User } from "@/back/generated/prisma_client"
 import Header from "../Global/Header"
+import { usePathname } from "next/navigation"
 
 export function AccountShell({ user, children }: { user: User | null; children: React.ReactNode }) {
+    const pathname = usePathname()
+    const isFixedLayout = pathname !== "/account" && pathname !== "/admin"
 
     return (
         <UserProvider user={user}>
             <div className="bg-[url('/images/bg2.jpg')] bg-cover bg-center">
-                <SidebarProvider className="isolate p-2 2xl:p-4">
+                <SidebarProvider className="gap-2 isolate p-2 2xl:p-4">
                     <Sheet>
-                        <AppSidebar />
-                        <SidebarInset className="bg-transparent!">
+                        <AppSidebar className={isFixedLayout ? "fixed left-2 z-50" : ""} />
+                        <SidebarInset className="relative bg-transparent!">
                             <Header />
-                            <main>{children}</main>
+                            <main className={isFixedLayout ? "pt-20" : ""}>{children}</main>
                         </SidebarInset>
                     </Sheet>
                 </SidebarProvider>
